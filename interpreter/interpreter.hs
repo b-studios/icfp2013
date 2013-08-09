2df4e06cb731d80ffb5ab46e11b0746293a1f728
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2013-08-09 11:41:10 +0200

    Represent programs as datatype.

--- interpreter/interpreter.hs
+++ interpreter/interpreter.hs
@@ -1,3 +1,24 @@
 module Main where
 
+-- only three variables per program anyway?!
+--
+-- data Id = Input | FoldBase | FoldStep
+
+type Id = String
+
+data P
+  = PLambda Id E
+
+data E
+  = Zero
+  | One
+  | Id
+  | If0 E E E
+  | Fold E E Id Id E
+  | Op1 Op1 E
+  | Op2 Op2 E E
+
+data Op1 = Not | Shl1 | Shr1 | Shr4 | Shr16
+data Op2 = And | Or | Xor | Plus
+
 main = putStrLn "hello world"
