77b00f0f5f955dbb44c1d7d8d580fe70a2d54077
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2013-08-09 11:50:08 +0200

    Compute size.

--- interpreter/interpreter.hs
+++ interpreter/interpreter.hs
@@ -19,4 +19,20 @@ data E
 data Op1 = Not | Shl1 | Shr1 | Shr4 | Shr16
 data Op2 = And | Or | Xor | Plus
 
+-- | Size of programs.
+
+sizeP :: P -> Int
+sizeP (Lambda e) = 1 + sizeE e
+
+-- | Size of expressions.
+
+sizeE :: E -> Int
+sizeE Zero = 1
+sizeE One = 1
+sizeE (Id _) = 1
+sizeE (If0 e1 e2 e3) = 1 + sizeE e1 + sizeE e2 + sizeE e3
+sizeE (Fold e1 e2 e3) = 2 + sizeE e1 + sizeE e2 + sizeE e3
+sizeE (Op1 _ e1) = 1 + sizeE e1
+sizeE (Op2 _ e1 e2) = 1 + sizeE e1 + sizeE e2
+
 main = putStrLn "hello world"
