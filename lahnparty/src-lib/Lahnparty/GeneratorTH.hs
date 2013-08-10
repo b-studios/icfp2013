d25d2e2a3c3cd33faed7bedee3bd190bf9409060
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-08-10 03:41:06 +0200

    Simplify shifts of 0
    See
    https://github.com/b-studios/icfp2013/commit/0553bfdf4eb91db0ac61d061ec83c9459409ac94#commitcomment-3831726

--- lahnparty/src-lib/Lahnparty/GeneratorTH.hs
+++ lahnparty/src-lib/Lahnparty/GeneratorTH.hs
@@ -24,6 +24,10 @@ isOp3 OpIf0 = True
 isOp3 OpFold = True
 isOp3 _ = False
 
+isShift :: Op1 -> Bool
+isShift Not = False
+isShift _ = True
+
 arity :: Op -> Int
 arity (OpOp1 _) = 1
 arity (OpOp2 _) = 2
@@ -91,6 +95,7 @@ findE n ops infold mustfold = if (n<5 && mustfold)
     gen :: Op -> [E]
     gen (OpOp1 op1) = [ Op1 op1 e0
                       | e0 <- findE (n-1) ops infold mustfold
+                      , not (isShift op1 && e0 == Zero) -- shifts on Zero always have smaller equivalents. PG
                       ]
     gen (OpOp2 op2) = if mustfold
                       -- XXX: in all the examples below, we generate all possible values of e1 again for each value of e0 - don't we? That's a waste, fixable by inserting lets.
