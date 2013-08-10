6da8345a5ddeaea6f47bc61d8c4366b094ccac02
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-08-10 22:47:49 +0200

    Tests: enable QuickCheck

--- lahnparty/src-test/Lahnparty/Test/Language.hs
+++ lahnparty/src-test/Lahnparty/Test/Language.hs
@@ -8,6 +8,8 @@ import Numeric
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit (assertEqual)
+import Test.QuickCheck
+import Test.Framework.Providers.QuickCheck2 (testProperty)
 
 import Lahnparty.Language
 
