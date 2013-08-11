454be1b8a0c73d2c3337cc40de29d2d74c144ca5
Jonathan <jonathan@b-studios.de>
2013-08-11 22:19:12 +0200

    Hotfix

--- distributed/server.rb
+++ distributed/server.rb
@@ -202,6 +202,8 @@ eos
 
     halt 410 if $current_problem.failed?
     halt 412 if $current_problem.solved?
+
+    halt 412 if worker.task == nil or worker.task.problem
     halt 412 if worker.task.problem.id != $current_problem.id # for sanity - already solved
   end
 
